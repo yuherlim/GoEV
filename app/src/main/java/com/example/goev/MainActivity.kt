516ac25a6fd3d2bd5825ef9be816d0fda03a4ab3
yuherlim <110385648+yuherlim@users.noreply.github.com>
2023-05-02 14:53:15 +0800

    reset to initial state

--- app/src/main/java/com/example/goev/MainActivity.kt
+++ app/src/main/java/com/example/goev/MainActivity.kt
@@ -7,18 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Test
-        //Master test
-        //Yu Her test merge to Master
-        //test
-        //jj test
-
-        //Test 3
-        //Test 4
-        //Test 5
-
-        //jjaiee
-
-        //navigation component
     }
 }
