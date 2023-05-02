fd942430c7626d1c36287ec58dbd28e5ee0a4672
Siays <sysheng1@hotmail.com>
2023-05-02 15:20:29 +0800

    add data binding

--- app/src/main/java/com/example/goev/MainActivity.kt
+++ app/src/main/java/com/example/goev/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.goev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.goev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
