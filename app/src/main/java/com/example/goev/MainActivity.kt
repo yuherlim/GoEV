68a31cf3fdb97826e09dac96febc479f80ab4327
khokajie <khokajie01@gmail.com>
2023-05-03 23:20:27 +0800

    set the binding var to private

--- app/src/main/java/com/example/goev/MainActivity.kt
+++ app/src/main/java/com/example/goev/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.databinding.DataBindingUtil
 import com.example.goev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
