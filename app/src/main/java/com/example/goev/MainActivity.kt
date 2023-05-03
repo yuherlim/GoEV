01d233d8d0ffeb49965eea267a9b620403483cd0
Siays <sysheng1@hotmail.com>
2023-05-04 07:39:29 +0800

    add tester button

--- app/src/main/java/com/example/goev/MainActivity.kt
+++ app/src/main/java/com/example/goev/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.goev
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import com.example.goev.databinding.ActivityMainBinding
 
@@ -12,6 +14,14 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
          // Testing
+
+
+        //temporarily test button
+        val button = findViewById<Button>(R.id.tkTest)
+        button.setOnClickListener {
+            val intent = Intent(this, TipsAndKnowledge::class.java)
+            startActivity(intent)
+        }
     }
 
 }
