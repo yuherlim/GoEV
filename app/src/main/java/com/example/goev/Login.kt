b270af17ac8f0241ed59055d485f4e0349ff89c2
khokajie <khokajie01@gmail.com>
2023-05-04 22:11:31 +0800

    done the register activity design

--- app/src/main/java/com/example/goev/Login.kt
+++ app/src/main/java/com/example/goev/Login.kt
@@ -3,16 +3,18 @@ package com.example.goev
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.example.goev.databinding.ActivityMainBinding
+import com.example.goev.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
+        binding.loginButton.setOnClickListener {
 
-
+        }
     }
 }
