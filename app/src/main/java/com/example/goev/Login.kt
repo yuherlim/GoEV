c5abfd249ba93ea79401a8282f37bccdf0ef7410
khokajie <khokajie01@gmail.com>
2023-05-04 17:00:37 +0800

    done the login activity design

--- app/src/main/java/com/example/goev/Login.kt
+++ app/src/main/java/com/example/goev/Login.kt
@@ -2,11 +2,17 @@ package com.example.goev
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.databinding.DataBindingUtil
+import com.example.goev.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+
+
     }
 }
