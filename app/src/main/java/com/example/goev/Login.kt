c664816c187cb670a0c017d39783c7c67404d8d6
khokajie <khokajie01@gmail.com>
2023-05-05 15:51:20 +0800

    setting xml error cant fix

--- app/src/main/java/com/example/goev/Login.kt
+++ app/src/main/java/com/example/goev/Login.kt
@@ -1,21 +1,15 @@
 package com.example.goev
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.example.goev.database.User
-import com.example.goev.database.UserDao
-import com.example.goev.database.UserDatabase
 import com.example.goev.databinding.ActivityLoginBinding
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class Login : AppCompatActivity() {
 
     private lateinit var binding : ActivityLoginBinding
-    private lateinit var userDb : UserDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,35 +17,10 @@ class Login : AppCompatActivity() {
 
         binding.loginButton.setOnClickListener {
 
-            checkUser()
+
 
         }
 
     }
-    private fun checkUser() {
 
-        // Get user input
-        val email = binding.editLoginEmail.text.toString()
-        val password = binding.editLoginPassword.text.toString()
-
-        // Validate user input
-        if (email.isBlank() || password.isBlank()) {
-            Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
-        } else {
-
-            lateinit var user : User
-
-            GlobalScope.launch {
-                user = userDb.userDao().getUserByEmail(email)!!
-                if (user.password == password) {
-                    // Navigate to main activity
-
-
-
-                } else {
-                    //Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/goev/Register.kt
+++ app/src/main/java/com/example/goev/Register.kt
@@ -1,13 +1,28 @@
 package com.example.goev
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.goev.databinding.ActivityRegisterBinding
 
 class Register : AppCompatActivity(){
 
+//    var name: String? = ""
+//    var email: String? = ""
+//    var password: String? = ""
+//    var reenterPassword: String? =""
+
     private lateinit var binding : ActivityRegisterBinding
+    private val URL: String = "http://10.0.2.2/dbconnects/register.php"
     
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -18,4 +33,52 @@ class Register : AppCompatActivity(){
 
         }
     }
+
+//    fun save(view: View?) {
+//        name = binding.editRegisterName.text.toString().trim { it <= ' ' }
+//        email = binding.editRegisterEmail.text.toString().trim { it <= ' ' }
+//        password = binding.editRegisterPassword.text.toString().trim { it <= ' ' }
+//        reenterPassword = binding.editRegisterPassword2.text.toString().trim { it <= ' ' }
+//        if (password != reenterPassword) {
+//            Toast.makeText(this, "Password Mismatch", Toast.LENGTH_SHORT).show()
+//        } else if (name != "" && email != "" && password != "") {
+//            val stringRequest: StringRequest = object : StringRequest(
+//                Request.Method.POST, URL,
+//                Response.Listener { response ->
+//                    binding.registerStatus.text = response.toString()
+//                    Log.d("Register",response)
+//                    if (response == "success") {
+//                        binding.registerStatus.text = "Successfully registered."
+//                        binding.registerButton.isClickable = false
+//                    } else if (response == "failure") {
+//                        binding.registerStatus.text = "Something went wrong!"
+//                    }
+//                },
+//                Response.ErrorListener { error ->
+//                    Toast.makeText(
+//                        applicationContext,
+//                        error.toString().trim { it <= ' ' },
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                }) {
+//                @Throws(AuthFailureError::class)
+//                override fun getParams(): Map<String, String>? {
+//                    val data: MutableMap<String, String> = HashMap()
+//                    data["name"] = name!!
+//                    data["email"] = email!!
+//                    data["password"] = password!!
+//                    return data
+//                }
+//            }
+//            val requestQueue = Volley.newRequestQueue(applicationContext)
+//            requestQueue.add(stringRequest)
+//        }
+//    }
+//
+//    fun login(view: View?) {
+//        val intent = Intent(this, MainActivity::class.java)
+//        startActivity(intent)
+//        finish()
+//    }
+
 }
