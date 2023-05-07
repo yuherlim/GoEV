a6d32bd8b2d6163a6239abf4e179a905343729ba
yuherlim <110385648+yuherlim@users.noreply.github.com>
2023-05-07 18:00:28 +0800

    Initial setup for navhost, safeargs, navgraph, and action bar

--- app/src/main/java/com/example/goev/MainActivity.kt
+++ app/src/main/java/com/example/goev/MainActivity.kt
@@ -2,16 +2,36 @@ package com.example.goev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.goev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-         // Testing
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        // Enable action bar
+//        setupActionBarWithNavController(navController)
+
+        val actionBar = getActionBar()
+        if (actionBar != null) {
+            Log.d("MainActivity", "Using default ActionBar")
+        } else {
+            Log.d("MainActivity", "Using custom Toolbar")
+        }
+
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
