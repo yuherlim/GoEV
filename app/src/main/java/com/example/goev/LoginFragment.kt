d7f91c1ef4ca569907d03dd76c94ed0dd9843091
khokajie <khokajie01@gmail.com>
2023-05-11 14:25:15 +0800

    done the login fragment and register fragment (havent implement the validation)

--- app/src/main/java/com/example/goev/LoginFragment.kt
+++ app/src/main/java/com/example/goev/LoginFragment.kt
@@ -1,79 +1,62 @@
 package com.example.goev
 
+import android.app.ProgressDialog.show
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.goev.databinding.ActivityLoginBinding
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
 
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val binding: ActivityLoginBinding = DataBindingUtil.inflate(inflater, R.layout.activity_login, container, false)
 
-        val email = binding.editLoginEmail.text
-        val password = binding.editLoginPassword.text
+        // Inflate the layout for this fragment
+        binding = ActivityLoginBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
         //button on click...
-        binding.loginButton.setOnClickListener {
-            validateUser(email.toString(), password.toString())
+        binding.loginButton.setOnClickListener {view: View ->
+
+            val email = binding.editLoginEmail.text.toString()
+            val password = binding.editLoginPassword.text.toString()
+
+            if(email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(requireContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            }
+
+            viewModel.loginValidation(email, password) { success ->
+                if (success) {
+                    // Navigate to next fragment
+                    Toast.makeText(requireContext(), "Login....", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(requireContext(), "Wrong email or wrong password", Toast.LENGTH_SHORT).show()
+                    clearInputFields()
+                }
+            }
+
+        }
+
+        binding.loginToSignText2.setOnClickListener{view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
         return binding.root
     }
 
-    private fun validateUser(email:String, password:String){
-        if()
+    private fun clearInputFields() {
+        binding.editLoginEmail.setText("")
+        binding.editLoginPassword.setText("")
     }
 
-
-
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/example/goev/MainActivity.kt
+++ app/src/main/java/com/example/goev/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.example.goev
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.goev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-         // Testing
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
 }
